8be3133712f402ad3353943fe9f422084a6b9082
kmeena01 <ctmeena@gmail.com>
2024-05-30 15:36:45 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Launch Chrome Browser //
+            IWebDriver driver = new ChromeDriver();
+            // step 2 - Navigate to Turnup portal //
+            // step 3 - Enter username value in username field //
+            // step 4 - Enter password value in password field //
+            // step 5 - Click Login Button //
+            // step 6 - Verify user login was successfull //
+        }
+    }
+}
